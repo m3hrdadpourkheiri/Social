e4cdd2f76c348d470e3a9798ed4177557518a311
Mehrdad Pourkheiri <m3hrdad.p@gmail.com>
2024-11-18 13:24:55 +0330

    user register

--- A/account/forms.py
+++ A/account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UserRegistrationFrom(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'your username'}),label='نام کاربری',error_messages={'required':'لطفا نلم کاربری را وارد کنید'})
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Example@email.com'}),label='ایمیل')
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'your password'}),label='کلمه عبور')
+    username = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'your username'}),label='نام کاربری')
+    email = forms.EmailField(required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Example@email.com'}),label='ایمیل')
+    password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'your password'}),label='کلمه عبور')
--- A/account/views.py
+++ A/account/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .forms import UserRegistrationFrom
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 
 class RegisterView(View):
-    def get(self,request):
-        form = UserRegistrationFrom()
-        return render(request,'account/register.html',{'form':form})
-    def post(self,request):
-        pass
+        form_class = UserRegistrationFrom
+
+        def get(self,request):
+            form = self.form_class()
+            return render(request,'account/register.html',{'form':form})
+        
+
+        def post(self,request):
+            
+            form = self.form_class(request.POST)
+            
+            if User.objects.filter(username = request.POST['username']).exists():
+                 messages.success(request,'Username alredy exist','info')
+                 return render(request,'account/register.html',{'form':form})
+
+
+            if form.is_valid():
+                cd = form.cleaned_data
+                User.objects.create_user(cd['username'],cd['email'],cd['password'])
+                messages.success(request,'you registered successfully','success')
+                return redirect('home:home')
+            else:
+                messages.success(request,"لطفا اطلاعات را به درسیتی وارد کنید.",'danger')
+                return render(request,'account/register.html',{'form':form})
